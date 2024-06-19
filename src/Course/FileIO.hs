1dd277aef25cfaaca9323254943cd8bf375b1084
hank <ysh2291939848@outlook.com>
2024-06-19 15:43:04 +0800

    finished fileio

--- src/Course/FileIO.hs
+++ src/Course/FileIO.hs
@@ -1,15 +1,16 @@
-{-# LANGUAGE NoImplicitPrelude #-}
-{-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE RebindableSyntax #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE NoImplicitPrelude #-}
 
 module Course.FileIO where
 
-import Course.Core
+import Control.Monad (Monad (return))
 import Course.Applicative
-import Course.Monad
+import Course.Core
 import Course.Functor
 import Course.List
+import Course.Monad
 
 {-
 
@@ -60,7 +61,7 @@ To test this module, load ghci in the root of the project directory, and do
 
 Example output:
 
-$ ghci
+\$ ghci
 GHCi, version ...
 Loading package...
 Loading ...
@@ -82,49 +83,58 @@ the contents of c
 -- Given the file name, and file contents, print them.
 -- Use @putStrLn@.
 printFile ::
-  FilePath
-  -> Chars
-  -> IO ()
-printFile =
-  error "todo: Course.FileIO#printFile"
+  FilePath ->
+  Chars ->
+  IO ()
+printFile fp c = putStrLn ("============ " ++ fp ++ "\n" ++ c)
 
 -- Given a list of (file name and file contents), print each.
 -- Use @printFile@.
 printFiles ::
-  List (FilePath, Chars)
-  -> IO ()
-printFiles =
-  error "todo: Course.FileIO#printFiles"
+  List (FilePath, Chars) ->
+  IO ()
+printFiles ls = void $ sequence $ map (uncurry printFile) ls
+-- printFiles = mapM_ (uncurry printFile)
+
+-- we could use mapM_
 
 -- Given a file name, return (file name and file contents).
 -- Use @readFile@.
 getFile ::
-  FilePath
-  -> IO (FilePath, Chars)
-getFile =
-  error "todo: Course.FileIO#getFile"
+  FilePath ->
+  IO (FilePath, Chars)
+getFile fp = do
+  c <- readFile fp
+  return (fp, c)
 
 -- Given a list of file names, return list of (file name and file contents).
 -- Use @getFile@.
 getFiles ::
-  List FilePath
-  -> IO (List (FilePath, Chars))
-getFiles =
-  error "todo: Course.FileIO#getFiles"
+  List FilePath ->
+  IO (List (FilePath, Chars))
+-- getFiles = mapM getFile
+getFiles ls = sequence $ map getFile ls
 
 -- Given a file name, read it and for each line in that file, read and print contents of each.
 -- Use @getFiles@, @lines@, and @printFiles@.
 run ::
-  FilePath
-  -> IO ()
-run =
-  error "todo: Course.FileIO#run"
+  FilePath ->
+  IO ()
+run fp = do
+  (_, fc) <- getFile fp
+  x <- getFiles $ lines fc
+  printFiles x
+
 
 -- /Tip:/ use @getArgs@ and @run@
 main ::
   IO ()
-main =
-  error "todo: Course.FileIO#main"
+main = do
+  args <- getArgs
+  case args of
+    Nil -> putStrLn "No file provided"
+    h :. _ -> run h
+
 
 ----
 
